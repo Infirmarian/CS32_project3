388025db642edb2829178020693224852811dbc3
Infirmarian <robert@rgeil.net>
2018-02-14 21:08:27 -0800

    Initial Commit

---
+++ project3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project3
+//
+//  Created by Robert Geil on 2/14/18.
+//  Copyright © 2018 Robert Geil. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
